7f397f24e957a4fc4fa1dc79d317ad376c8b2c35
CarlosUPC <wolfsea.yt@gmail.com>
2018-10-21 21:55:53 +0200

    sync problem solved

--- XRollin'Pinball/Motor2D/ModuleSceneIntro.h
+++ XRollin'Pinball/Motor2D/ModuleSceneIntro.h
@@ -35,15 +35,14 @@ public:
 	SDL_Texture* startgame_texture = nullptr;
 	bool game_started = false;
 
-<<<<<<< HEAD
-	SDL_Texture* left_flipper;
-	SDL_Texture* right_flipper;
 
-	SDL_Texture* Upleft_flipper;
-	SDL_Texture* Upright_flipper;
-=======
+	
+
+	SDL_Texture* Upleft_flipper = nullptr;
+	SDL_Texture* Upright_flipper = nullptr;
+
 	SDL_Texture* left_flipper = nullptr;
 	SDL_Texture* right_flipper = nullptr;
->>>>>>> 6def48e9e46125602212d1da7431400c6c208c73
+
 	uint bonus_fx;
 };
